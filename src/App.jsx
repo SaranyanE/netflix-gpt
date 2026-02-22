91316ddf52a5b0329d2e943a5faf6bd985ebbae2
Saranyan.E <saran201496@gmail.com>
2026-02-22 16:04:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
